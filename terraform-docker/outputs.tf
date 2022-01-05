c92fefc1538b941f2e822980bc80bbaa78013df8
sanjay <sanjayrohilla@gmail.com>
2022-01-05 14:20:24 +1100

    container module added with outputs

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -7,3 +7,14 @@
 #   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
 #   #sensitive = true
 # }
+# moved the output to module output
+
+output "container-name" {
+  description = "The name of the container"
+  value       = module.container[*].container-name 
+  }
+
+output "ip-address" {
+  description = "The ip address"
+  value       = flatten(module.container[*].ip-address)      
+}
