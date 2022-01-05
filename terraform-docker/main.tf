d6b31bda5062600ccdbb9b516103b91cf53e5d1a
sanjay <sanjayrohilla@gmail.com>
2022-01-05 16:32:37 +1100

    creating multiple images with one image module

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,9 +9,14 @@
 #   name = "nodered/node-red:latest"
 # }
 # use module for pulling docker image
-module "image" {
+module "nodered_image" {
   source = "./image"
-  image_in = var.image[terraform.workspace]
+  image_in = var.image["nodered"][terraform.workspace]
+}
+
+module "influxdb_image" {
+  source = "./image"
+  image_in = var.image["influxdb"][terraform.workspace]
 }
 #generate a random string for docker name
 resource "random_string" "random" {
@@ -52,9 +57,8 @@ module "container" {
   # dockervol is moved to container module
   count = local.container_count
   name_in  = join("-", ["nodered", terraform.workspace ,random_string.random[count.index].result])
-  image_in = module.image.image_out
+  image_in = module.nodered_image.image_out
   int_port_in = var.int_port
   ext_port_in = var.ext_port[terraform.workspace][count.index]
   container_path_in = "/data"
-  host_path_in = "${path.cwd}/noderedvol"
 }
--- terraform-docker/variables.tf
+++ terraform-docker/variables.tf
@@ -10,8 +10,14 @@ variable "image" {
   type        = map(any)
   description = "The list of docker images for various enviornments"
   default = {
+  nodered = {
     dev  = "nodered/node-red:latest"
     prod = "nodered/node-red:latest-minimal"
+  }
+  influxdb = {
+    dev  = "quay.io/influxdb/influxdb:v2.0.2"
+    prod = "quay.io/influxdb/influxdb:v2.0.2"
+  }
   }
 }
 variable "int_port" {
